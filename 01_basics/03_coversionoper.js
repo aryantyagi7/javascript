024c31291ebbc6c84a4eb2e0074e54c2da621e8c
aryantyagi7 <tyagiaryan1201@gmail.com>
2026-09-23 14:59:26 +0000

    commit

--- 01_basics/03_coversionoper.js
+++ 01_basics/03_coversionoper.js
@@ -1,7 +1,18 @@
-let score =33
-console.log(typeof score);
-let valueinnumber = Number(score)
-console.log (valueinumber);
-// "33abc"=>nan type number
-let isloggedin =1
-let booleanisloggedin =boolean(isloggedin )
+// let score =33
+// console.log(typeof score);
+// let valueinnumber = Number(score)
+// console.log (valueinumber);
+// // "33abc"=>nan type number
+// let isloggedin =1
+// let booleanisloggedin =boolean(isloggedin )
+
+// let value =3
+// let negvalue = -value 
+
+// let str1 ="hello"
+// let str2 ="ar"
+// let str3 =str1+str2
+
+// let number = 100
+// number++;
+// console.log(number);
