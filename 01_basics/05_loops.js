f6712449b307c50ef3f63b5387f671703d3e189f
aryantyagi7 <tyagiaryan1201@gmail.com>
2026-09-24 08:57:58 +0000

    commit

---
+++ 01_basics/05_loops.js
@@ -0,0 +1,53 @@
+// for(let i =1;i<=5;i++){
+// console.log("aryan");
+// }
+
+//sum of first n numberes
+// let sum=0;
+// for (let i = 0; i <=5;i++) {
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+// let i=1;
+// while(i<5){
+//     console.log(i);
+//     i++;
+    
+// }
+// let i =1
+// do{
+//     console.log("aryan");
+//     i++;
+    
+// }while (i<=2) {
+    
+// }
+// let student={
+//     name:"arya",
+//     age: 20,
+//     cgpa: 7.5,
+//     ispass:true
+// };
+
+// for(let key in student){ //it returns key 
+//     console.log(student[key]);
+    
+// }
+// for (let index = 0; index <=100; index++) {
+//     if (index%2==0) {
+//         console.log(index);
+        
+//     }
+// }
+// let gamenumber =25;
+// let usernumber = Prompt("guess the game number");
+// while(usernumber!=gamenumber){
+//     usernumber= Prompt("you lose");
+// }
+
+// console.log("winner winner chicken dinner");
+
+
+
+ 
---
+++ 01_basics/06_strings.js
@@ -0,0 +1,13 @@
+// let str="aryan";
+// console.log(str.length);
+//function=> methods
+// // template literals
+// let sentence=`this is a template literal`
+// console.log(typeof(sentence));
+// let str ="aryan";
+// str = str.toUpperCase();
+//  console.log(str); strings are immutable 
+// str1+str2 for concatination
+// newstr = str1.concat(str2)
+// str.charat()
+
