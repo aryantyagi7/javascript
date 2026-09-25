07957af52ce5b5274b44567cb3fe5e108a3fb086
aryantyagi7 <tyagiaryan1201@gmail.com>
2026-09-25 12:18:58 +0000

    commit

---
+++ 01_basics/07_functions.js
@@ -0,0 +1,58 @@
+// functions are block of code which can be invoked whenever you want 
+// function myfunc() {
+//     console.log("welcome");
+//     console.log("js king ");
+// }
+// myfunc();
+// function sum(x,y){
+//     s = x+y;
+//     return s;
+// }
+// let val =sum(3,4);
+// console.log(val);
+
+// const arrowmul=(x,y)=>{
+//     return x*y;
+    
+// };
+
+// arrowmul(3,4);
+// function countvowels(str){
+//      for(const char of str){
+//         console.log(char);
+// }
+// }
+// countvowels("helloe");  
+// const countvow=(str)=>{
+//     let count=0;
+//     for(const char of str ){
+//         if ( char==='a'||char==='e'||char==='i'||char==='o'||char==='u'){
+//             count++;
+//         }
+//     }
+//    return count;
+
+// }
+//   countvow("hellow"); 
+//  let nums=[1,2,3,4,5];
+//  nums.forEach((num) => {  
+//     console.log(num*num);
+    
+    
+//  });
+// map method is use to make new arr using the values of existing arr
+//  let nums=[1,2,3,4,5];
+//  let newarr=nums.map((val)=>{
+//      return val**2;
+// }); 
+// console.log(newarr);
+
+// let nums=[1,2,3,4,5];
+//  let newarr=nums.filter((val)=>{
+//       return val%2===0;
+//  });
+//  console.log(newarr);
+ 
+
+
+
